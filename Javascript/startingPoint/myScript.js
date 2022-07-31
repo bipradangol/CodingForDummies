f40e630069984af303ac20a9ea35ac86d0d144a7
Bipra Dangol <bipdangol@outlook.com>
2022-07-31 15:51:11 -0400

    Function is Javascript.

--- Javascript/startingPoint/myScript.js
+++ Javascript/startingPoint/myScript.js
@@ -1,20 +1,20 @@
 var x = 10;
 console.log(x);
 
-function bipra(a, b) {
+function bipadd(a, b) {
     var sum = 0;
     sum = a + b;
     return sum;
 }
 
-function binit(a, b) {
+function binsub(a, b) {
     return a - b;
 }
 
-var total = bipra(10, 20);
+var total = bipadd(10, 20);
 console.log(total);
 
-console.log(binit(30, 12));
+console.log(binsub(30, 12));
 
 function sayHi(name) {
     console.log('Hi ' + name);
@@ -50,7 +50,7 @@ document.addEventListener('DOMContentLoaded', function() {
 }, false);
 
 //doMath with Named Functions
-function doMath(number1,number2,callback) {
+function doMath2(number1,number2,callback) {
     if (typeof callback === "function") {
 
         var result = callback(number1,number2);
@@ -71,7 +71,78 @@ function modThem(number1,number2) {
     return calculation;
 }
 document.addEventListener('DOMContentLoaded', function() {
-    doMath(5,2,multiplyThem);
-    doMath(10,3,divideThem);
-    doMath(81,9,modThem);
+    doMath2(5,2,multiplyThem);
+    doMath2(10,3,divideThem);
+    doMath2(81,9,modThem);
 },false);
+
+function add(){
+    doMath2(5, 2, function (number1, number2) {
+            var calculation = number1 + number2;
+            return calculation;
+        }
+    );
+}
+
+function del() {
+    document.getElementById("theResult").innerHTML = "";
+    document.getElementById("theResult2").innerHTML = "";
+}
+
+function changeBackground() {
+    colors = ["red", "green", "blue"];
+    var randomNumber = Math.floor(Math.random()*colors.length);
+    document.body.style.backgroundColor = colors[randomNumber];
+}
+
+function addTwoNumbers() {
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var result = Number(num1) + Number(num2);
+    document.getElementById("result1").value = result;
+    console.log(result);
+}
+
+function subtractTwoNumbers() {
+    var num1 = document.getElementById("numSub1").value;
+    var num2 = document.getElementById("numSub2").value;
+    var result = Number(num1) - Number(num2);
+    document.getElementById("result2").value = result;
+    console.log(result);
+}
+
+function addUp() {
+    var inputString = document.getElementById("input").value;
+    var input = Number(inputString);
+    var sum = 0;
+    for (let i = 1; i <= input; i++) {
+        sum = sum + i;
+    }
+    document.getElementById("answer").value = sum;
+}
+
+function findFibonacciNum() {
+    var inputOriginal = document.getElementById("insertNum").value;
+    var input = Number(inputOriginal);
+
+    var fibNum = getFibNumber(input);
+    console.log(fibNum);
+}
+
+function getFibNumber(input) {
+    var first = 0;
+    var second = 1;
+    var fibonacci = 0;
+    if (input === 1) {
+        return 0;
+    } else if (input === 2) {
+        return 1;
+    } else {
+        for (let i = 3; i <= input; i++) {
+            fibonacci = first + second;
+            first = second;
+            second = fibonacci;
+        }
+    }
+    return fibonacci;
+}
