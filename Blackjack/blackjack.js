92d212cecc86563b642f4f0e0055e42b1f62ee88
Bipra Dangol <bipdangol@outlook.com>
2022-08-19 13:57:22 -0400

    Added a cookie to save the current amount.

--- Blackjack/blackjack.js
+++ Blackjack/blackjack.js
@@ -15,6 +15,8 @@ var backCard
 let currentAmount = 0;
 
 window.onload = function () {
+    currentAmount = checkCookie();
+    document.getElementById("currentAmount").innerText = currentAmount < 0 ? "-$" + Math.abs(currentAmount) : "$" + currentAmount;
     createBackCardOriginal();
     initializeGame();
 }
@@ -169,6 +171,7 @@ function stay() {
         currentAmount -= 100;
     }
 
+    setCookie("currentAmount", currentAmount, 1);
     document.getElementById("results").innerText = message;
     document.getElementById("currentAmount").innerText = currentAmount < 0 ? "-$" + Math.abs(currentAmount) : "$" + currentAmount;
     document.getElementById("dealer-sum").innerText = dealerSum;
@@ -202,4 +205,33 @@ function reduceAce(playerSum, playerAceAccount) {
         playerAceAccount -= 1;
     }
     return playerSum;
+}
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    let cAmount = getCookie("currentAmount");
+    console.log("cookie: ", cAmount);
+    return Number(cAmount);
+
 }
